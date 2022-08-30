f7f1aa21757090971aa669f6306eb0fa7d2af980
ebil <you@exaple.com>
2022-08-30 13:41:05 +0100

    Implemented preflop card generation

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,36 @@ struct Hand {
 }
 
 impl Hand {
-    fn new() -> Hand {
+    fn new(used_cards: &mut Vec<u8>) -> Hand {
         let mut rng = thread_rng();
-        let mut first_card: u8 = 0;
-        let mut second_card: u8 = 0;
+        let mut card: u8 = 0;
 
         let mut i: u8 = 0;
-        while i < 1 {
-            first_card = rng.gen_range(1..53);
-            second_card = rng.gen_range(1..53);
+        while i < 2 {
+            card = rng.gen_range(1..53);
 
-            if first_card != second_card {
+            if used_cards.contains(&card) {
+                println!("Card already used");
+            } else {
+                used_cards.push(card);
                 i += 1;
             }
         }
-        return Hand { card_one: first_card, card_two: second_card };
+        let len_used_cards = used_cards.len();
+        return Hand {
+            card_one: used_cards[len_used_cards - 1],
+            card_two: used_cards[len_used_cards - 2]
+        };
     }
 }
 
 fn main() {
-    let used_cards: Vec<u8> = Vec::new();
+    let mut used_cards: Vec<u8> = Vec::new();
 
-    // Compare structs with PartialEq: https://turreta.com/2019/09/15/rust-how-to-compare-struct-instances/
-    let player = Hand::new();
-    let computer = Hand::new();
-
+    let player = Hand::new(&mut used_cards);
+    let computer = Hand::new(&mut used_cards);
 
     println!("{:?}", player);
     println!("{:?}", computer);
+    println!("{:?}", used_cards);
 }
